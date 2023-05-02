985a740ceb62a79c493d7726c6234f59a8a9a5e8
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-02 20:34:45 +0900

    ...

--- p04_linked_list/src/lib.rs
+++ p04_linked_list/src/lib.rs
@@ -1,23 +1,34 @@
 #[derive(Clone)]
 pub enum Address {
-    Address(Box<MyList>),
+    Address(Box<MyNode>),
     Nil,
 }
 
 #[derive(Clone)]
 pub struct MyList {
-    pub value: u32,
+    pub head: MyNode,
+    pub tail: MyNode,
+    pub length: u32,
+}
+
+
+impl MyList {
+}
+
+#[derive(Clone)]
+pub struct MyNode {
+    pub value: i32,
     pub next: Address,
 }
 
-impl MyList {
-    pub fn append(&mut self, elem: u32) {
+impl MyNode {
+    pub fn append(&mut self, elem: i32) {
         match self.next {
             Address::Address(ref mut next_address) => {
                 next_address.append(elem);
             }
             Address::Nil => {
-                let node = MyList {
+                let node = MyNode {
                     value: elem,
                     next: Address::Nil,
                 };
@@ -26,7 +37,7 @@ impl MyList {
         }
     }
     
-    pub fn delete(&mut self, elem: u32) {
+    pub fn delete(&mut self, elem: i32) {
         match self.next {
             Address::Address(ref mut next_address) => {
                 if next_address.value == elem {
@@ -48,7 +59,7 @@ impl MyList {
 
     pub fn count(&self) -> u32 {
         match self.next {
-            Address::Address(ref newaddress) => 1 + newaddress.count(),
+            Address::Address(ref new_address) => 1 + new_address.count(),
             Address::Nil => 1,
         }
     }
@@ -65,7 +76,7 @@ impl MyList {
         }
     }
 
-    pub fn update(&mut self, index: u32, elem: u32) {
+    pub fn update(&mut self, index: u32, elem: i32) {
         let mut i = 0;
         let mut j = self;
         if i == index {
