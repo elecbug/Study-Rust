9c01d252825288c61cb4438766c44368c75874e8
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-03 13:40:20 +0900

    ...

--- p04_linked_list/src/lib.rs
+++ p04_linked_list/src/lib.rs
@@ -1,24 +1,38 @@
+
 #[derive(Clone)]
-pub enum Address<T> {
-    Address(Box<MyNode::<T>>),
-    Null,
+pub struct MyNode<T> {
+    value: Option<T>,
+    next: Option<Box<MyNode::<T>>>,
 }
 
 #[derive(Clone)]
 pub struct MyList<T> {
-    head: MyNode::<T>,
-    tail: MyNode::<T>,
+    head: Box<MyNode::<T>>,
+    tail: Box<MyNode::<T>>,
     length: u32,
 }
 
-#[derive(Clone)]
-pub struct MyNode<T> {
-    value: T,
-    next: Address<T>,
-}
-
 impl<T> MyNode<T> {
+    fn new() -> MyNode<T> {
+        MyNode { 
+            value: None,
+            next: None,
+        }
+    }
 }
 
 impl<T> MyList<T> {
+    pub fn new() -> MyList<T> {
+        let tail = Box::new(MyNode::<T>::new());
+        let head = Box::new(MyNode {
+            value: None,
+            next: Some(tail),
+        });
+
+        MyList {
+            head,
+            tail,
+            length: 0,
+        }
+    }
 }
