e9db5b02e1d065008b811d410cca5079d7e1f1a4
elecbug <deveb1479@gmail.com>
2024-03-01 20:07:16 +0900

    test serde app

---
+++ backend/240301/form_handling/src/main.rs
@@ -0,0 +1,41 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+use actix_web::web::{Either, Json, Form};
+
+// <easy-form-handling>
+#[derive(Deserialize)]
+struct Register {
+    username: String,
+    country: String,
+}
+
+// register form is JSON
+async fn json_register(form: web::Json<Register>) -> impl Responder {
+    format!("Hello {} from {}!", form.username, form.country)
+}
+
+// register form can be either JSON or URL-encoded
+async fn register(form: Either<Json<Register>, Form<Register>>) -> impl Responder {
+    let Register { username, country } = form.into_inner();
+    format!("Hello {username} from {country}!")
+}
+// </easy-form-handling>
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/form.html"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/register", web::post().to(register))
+            .route("/json_register", web::post().to(json_register))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
